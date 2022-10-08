902c72c66cb64391377607beb14c1906f249c8f2
nate-d3v <nate.d3v@gmail.com>
2022-10-08 16:08:32 -0300

    Modify comparepoaps function to return object

--- app/web3/poap.ts
+++ app/web3/poap.ts
@@ -15,25 +15,55 @@ async function comparePoaps(address1: string, address2: string) {
 
   const res2 = await data2.json();
 
-  const arr1 = [];
+  const obj1: any = {};
 
   for (const el in res1) {
     if (res1.hasOwnProperty(el)) {
-      arr1.push(res1[el].event.id);
+      let id = res1[el].event.id;
+      obj1[id] = {
+        id: id,
+        name: res1[el].event.name,
+        description: res1[el].event.description,
+        image_url: res1[el].event.image_url,
+      };
     }
   }
 
-  const arr2: any[] = [];
+  const obj2: any = {};
 
   for (const el in res2) {
     if (res2.hasOwnProperty(el)) {
-      arr2.push(res2[el].event.id);
+      let id = res2[el].event.id;
+      obj2[id] = {
+        id: id,
+        name: res2[el].event.name,
+        description: res2[el].event.description,
+        image_url: res2[el].event.image_url,
+      };
     }
   }
 
-  const common = arr1.filter((x) => arr2.includes(x) && x);
+  const common: any = [];
+
+  if (Object.keys(obj1).length >= Object.keys(obj2).length) {
+    for (let el in obj1) {
+      if (obj2.hasOwnProperty(el)) {
+        common.push(obj1[el]);
+      }
+    }
+  } else {
+    for (let el in obj2) {
+      if (obj1.hasOwnProperty(el)) {
+        common.push(obj2[el]);
+      }
+    }
+  }
+
+  console.log(common);
 
   return common;
 }
+
+comparePoaps("nate-d3v.eth", "cristianvaldivia.eth");
 
 export { comparePoaps };
