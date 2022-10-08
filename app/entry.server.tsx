60e2f15cdd779e4299b8ef3f1f287eb83b09475d
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-10-07 21:01:05 -0500

    feat: finish configuration for chakra ui

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,48 +1,44 @@
-import { PassThrough } from "stream";
-import type { EntryContext } from "@remix-run/node";
-import { Response } from "@remix-run/node";
-import { RemixServer } from "@remix-run/react";
-import { renderToPipeableStream } from "react-dom/server";
+// entry.server.tsx
+import { renderToString } from 'react-dom/server'
+import { CacheProvider } from '@emotion/react'
+import createEmotionServer from '@emotion/server/create-instance'
+import { RemixServer } from '@remix-run/react'
+import type { EntryContext } from '@remix-run/node' // Depends on the runtime you choose
 
-const ABORT_DELAY = 5000;
+import { ServerStyleContext } from './context'
+import createEmotionCache from './createEmotionCache'
 
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext
+  remixContext: EntryContext,
 ) {
-  return new Promise((resolve, reject) => {
-    let didError = false;
+  const cache = createEmotionCache()
+  const { extractCriticalToChunks } = createEmotionServer(cache)
 
-    const { pipe, abort } = renderToPipeableStream(
-      <RemixServer context={remixContext} url={request.url} />,
-      {
-        onShellReady: () => {
-          const body = new PassThrough();
+  const html = renderToString(
+    <ServerStyleContext.Provider value={null}>
+      <CacheProvider value={cache}>
+        <RemixServer context={remixContext} url={request.url} />
+      </CacheProvider>
+    </ServerStyleContext.Provider>,
+  )
 
-          responseHeaders.set("Content-Type", "text/html");
+  const chunks = extractCriticalToChunks(html)
 
-          resolve(
-            new Response(body, {
-              headers: responseHeaders,
-              status: didError ? 500 : responseStatusCode,
-            })
-          );
+  const markup = renderToString(
+    <ServerStyleContext.Provider value={chunks.styles}>
+      <CacheProvider value={cache}>
+        <RemixServer context={remixContext} url={request.url} />
+      </CacheProvider>
+    </ServerStyleContext.Provider>,
+  )
 
-          pipe(body);
-        },
-        onShellError: (err) => {
-          reject(err);
-        },
-        onError: (error) => {
-          didError = true;
+  responseHeaders.set('Content-Type', 'text/html')
 
-          console.error(error);
-        },
-      }
-    );
-
-    setTimeout(abort, ABORT_DELAY);
-  });
+  return new Response(`<!DOCTYPE html>${markup}`, {
+    status: responseStatusCode,
+    headers: responseHeaders,
+  })
 }
