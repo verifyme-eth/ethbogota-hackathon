df0aee269d99929626b89775be0db3426ec22dee
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-10-08 00:41:24 -0500

    feat: starting with action in verified

--- app/routes/verified.tsx
+++ app/routes/verified.tsx
@@ -1,9 +1,9 @@
-import type { LoaderFunction } from "@remix-run/node";
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 
 import { db } from "~/utils/db.server";
 
-import { Center, Stack, Text } from "@chakra-ui/react";
-import { useLoaderData } from "@remix-run/react";
+import { Button, Center, Stack, Text } from "@chakra-ui/react";
+import { useLoaderData, useSubmit } from "@remix-run/react";
 
 export const loader: LoaderFunction = async () => {
   let verified;
@@ -30,8 +30,35 @@ export const loader: LoaderFunction = async () => {
   return { verified };
 };
 
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+
+  const address = form.get("address");
+
+  console.log(address);
+
+  if (!address || typeof address !== "string") return null;
+
+  return null;
+};
+
 export default function Verified() {
   const { verified } = useLoaderData();
+
+  const submit = useSubmit();
+
+  const handleVerify = async () => {
+    const formData = new FormData();
+
+    formData.append("address", "0x3aeC2276326CDC8E9a8A4351c338166e67105AC3");
+
+    submit(formData, {
+      action: "/verified/?index",
+      method: "post",
+      encType: "application/x-www-form-urlencoded",
+      replace: true,
+    });
+  };
 
   return (
     <Center>
@@ -47,6 +74,20 @@ export default function Verified() {
         <Text fontSize="24px" fontWeight={700} mt="30" color="third">
           Indice de verificación: {verified?.indexVM} %
         </Text>
+
+        <Center>
+          <Button
+            bg={"first"}
+            color="white"
+            borderRadius={70}
+            mt="300px"
+            onClick={handleVerify}
+          >
+            <Text fontWeight={700} fontSize={"18px"} lineHeight={"21.6px"}>
+              Verificar
+            </Text>
+          </Button>
+        </Center>
       </Stack>
     </Center>
   );
