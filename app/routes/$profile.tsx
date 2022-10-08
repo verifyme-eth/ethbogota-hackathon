23b1b3a72a557221ef74cd6b7c2b0647944176cc
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-10-08 03:20:14 -0500

    feat: getting profile from lens

--- app/routes/$profile.tsx
+++ app/routes/$profile.tsx
@@ -1,9 +1,40 @@
-import { Box, Text } from "@chakra-ui/react";
+import type { LoaderFunction } from "@remix-run/server-runtime";
+
+import { Box, Center, Text } from "@chakra-ui/react";
+import { useLoaderData } from "@remix-run/react";
+import { GraphQLClient } from "graphql-request";
+import { GetProfile } from "~/web3/lens";
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const lens = new GraphQLClient("https://api.lens.dev/playground");
+
+  const variables = {
+    request: { handle: params.profile },
+  };
+
+  const response = await lens.request(GetProfile, variables);
+
+  const userProfile = response.profile;
+
+  return { userProfile };
+};
 
 export default function Profile() {
+  const { userProfile } = useLoaderData();
+
+  console.log(userProfile);
+
   return (
-    <Box>
-      <Text>Profile</Text>
-    </Box>
+    <Center>
+      <Box>
+        <Text fontSize="25px" fontWeight="bold" color="first">
+          {userProfile.name}
+        </Text>
+
+        <Text fontSize="20px" fontWeight="light" color="second">
+          @{userProfile.handle}
+        </Text>
+      </Box>
+    </Center>
   );
 }
